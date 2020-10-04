c37446b02e33098c625139dc45a5a6a3b9d85253
max larner <06mlarner@brightoncollege.net>
2020-10-04 10:01:19 -0700

    have completely re-written code to be object oriented - Can place more than one hold Cons: - Holds are currently placing no where near the mouse click, likely as i've made changes on how they're placed on the wall panel. Must fix.
    To Do:
    - Fix hold placements

--- Computer Science Coursework/Form1.cs
+++ Computer Science Coursework/Form1.cs
@@ -15,6 +15,7 @@ using System.Drawing.Drawing2D;
 
 namespace Computer_Science_Coursework
 {
+    
     public partial class Form1 : Form
     {
         int HoldCount = 0;
@@ -22,10 +23,13 @@ namespace Computer_Science_Coursework
         bool Red = false;
         bool Green = false;
         bool Blue = false;
+        string HoldColour;
 
         //Defines the wall panel 
         Panel WallPanel = new Panel();
         //Defines the picture boxes for each hold "is there a better way to do this? revisit..."
+        PictureBox pctBox_CurrentHold = new PictureBox();
+
         PictureBox pctBox_GreenHold = new PictureBox();
         PictureBox pctBox_BlueHold = new PictureBox();
         PictureBox pctBox_RedHold = new PictureBox();
@@ -65,147 +69,141 @@ namespace Computer_Science_Coursework
             Controls.Add(WallPanel);
         }
 
-        private void AddHold(int x, int y, PictureBox pctBox_Hold)
+       
+        // Onclick event for WallPanel
+        private void WallPanel_Click(object sender, MouseEventArgs e)
         {
-            //Asigns the picturebox passed in (the Hold) a numbered name 
-            pctBox_Hold.Name = "pctBox_Hold" + Convert.ToString(HoldCount);
+
+            //Creates the name of the new hold 
+            string HoldName = "pctBox_Hold" + Convert.ToString(HoldCount);
+            //Dectects the X and Y co-ordinates of the click and assigns them to variables
+            int xMouseClick = e.X;
+            int yMouseClick = e.Y;
+
+            // Creates an instance of the hold class 
+            Hold NewHold = new Hold(HoldName, HoldColour);
+            PictureBox pctBox_Temp = new PictureBox();
+            //assigns the temporary picturebox as the picturebox created via CreateHold()
+            pctBox_CurrentHold = NewHold.CreateHold();
+            NewHold.AddHold(yMouseClick, xMouseClick, pctBox_CurrentHold, WallPanel, HoldCount);
+        }
+        
+        //onclick event and paint event for every button... There must be a better way?? investigate using OOP to solve problem
+        public void pctBox_GreenHoldButton_Click(object sender, EventArgs e)
+        {
+            HoldColour = "Green";
+        }
+        public void pctBox_BlueHoldButton_Click(object sender, EventArgs e)
+        {
+            HoldColour = "Blue";
+        }
+        private void pctBox_RedHoldButton_Click(object sender, EventArgs e)
+        {
+            HoldColour = "Red";
+        }
+    }
+    /// Creating a class for holds 
+    public class Hold
+    {
+        PictureBox pctBox_CurrentHold = new PictureBox();
+        public string HoldName;
+        public string HoldColour;
+        public Point[] HoldShape;
+
+
+        public Hold(string Name, string Colour)
+            {
+                HoldName = Name;
+                HoldColour = Colour;
+            }
+
+        public PictureBox CreateHold()
+        {
+            PictureBox pctBox_Hold = new PictureBox();
+            pctBox_Hold.Name = HoldName;
+            // deciding colour of hold
+                if (HoldColour == "Red")
+                {
+                    pctBox_Hold.BackColor = Color.Red;
+                }
+                else if (HoldColour == "Blue")
+                {
+                    pctBox_Hold.BackColor = Color.Blue;
+                }
+                else
+                {
+                    pctBox_Hold.BackColor = Color.Green;
+                }
+            pctBox_CurrentHold = pctBox_Hold;
+            return pctBox_Hold;
+            
+        }
+        public void AddHold(int x, int y, PictureBox pctBox_Hold, Panel WallPanel, int HoldCount)
+        {
             //Assigns the location of the picturebox on the wall, position passed in from the WallPanel Click event
             pctBox_Hold.Location = new Point(x, y);
             //increases the hold count to make sure the next hold added has a unique name
             HoldCount++;
             //Adds the hold to the WallPanel
             WallPanel.Controls.Add(pctBox_Hold);
+            pctBox_Hold.Paint += new PaintEventHandler(pctBox_Hold_Paint);
+
+
         }
-        // Onclick event for WallPanel
-        private void WallPanel_Click(object sender, MouseEventArgs e)
+
+        public void pctBox_Hold_Paint(object sender, PaintEventArgs e)
         {
-            //Dectects the X and Y co-ordinates of the click and assigns them to variables
-            int xMouseClick = e.X;
-            int yMouseClick = e.Y;
+            GraphicsPath pctBox_Path = new GraphicsPath();
 
-            //Used for my own testing perpouses, not a feature 
-            MessageBox.Show(Convert.ToString(xMouseClick), Convert.ToString(yMouseClick));
-
-            //Creates a temporary picturebox that can be assigned the value of a hold picturebox... 
-            //...This is so any picture box can be passed into the add hold function
-            PictureBox pctBox_Temp = new PictureBox();
             
-            if (Blue == true)
+
+            //Select Case for shape based on colours
+            switch (HoldColour)
             {
-                pctBox_Temp = pctBox_BlueHold;
-                //paints the picturebox with a unique shape to the Form
-                //the picturebox itself is added to the panel in the AddHold() Function
-                pctBox_Temp.Paint += new PaintEventHandler(pctBox_BlueHold_Paint);
-            }
-            else if (Green == true)
-            {
-                pctBox_Temp = pctBox_GreenHold;
-                pctBox_Temp.Paint += new PaintEventHandler(pctBox_GreenHold_Paint);
-            }
-            else if (Red == true)
-            {
-                pctBox_Temp = pctBox_RedHold;
-                pctBox_Temp.Paint += new PaintEventHandler(pctBox_RedHold_Paint);
-            }
-
-            AddHold(xMouseClick, yMouseClick, pctBox_Temp);
-        }
-        
-        //onclick event and paint event for every button... There must be a better way?? investigate using OOP to solve problem
-        public void pctBox_GreenHoldButton_Click(object sender, EventArgs e)
-        { 
-            //When the Greenhold button has been selected, it sets Green to true and others to false 
-            Green = true;
-            Red = false;
-            Blue = false;
-        }
-
-        private void pctBox_GreenHold_Paint(object sender, PaintEventArgs e)
-        {
-            pctBox_GreenHold.BackColor = Color.Green;
-
-            //Creates a new graphics path that the hold shape can be added to
-            GraphicsPath pctBox_Path = new GraphicsPath();
-
-            // Creates a polygon using an array of points called GreenShape
-
-            Point[] GreenShape =
-                {
-                    new Point(23, 20),
-                    new Point(40, 10),
-                    new Point(57, 20),
-                    new Point(50, 40),
-                    new Point(30, 40)
-                    };
-
-            // Adds the polygon to the graphics path
-            pctBox_Path.AddPolygon(GreenShape);
-
-            //the region of the picturebox is now set to the shape of the graphics path
-            pctBox_GreenHold.Region = new Region(pctBox_Path);
-        }
-
-        public void pctBox_BlueHoldButton_Click(object sender, EventArgs e)
-        {
-            Green = false;
-            Blue = true;
-            Red = false;
-        }
-
-        private void pctBox_BlueHold_Paint(object sender, PaintEventArgs e)
-        {
-            pctBox_BlueHold.BackColor = Color.Blue;
-
-            GraphicsPath pctBox_Path = new GraphicsPath();
-
-            // The points of the rhombus in n array, creates the polygon
-            Point[] Rhombus =
-                {
-                    new Point(-2, 12),
-                    new Point(32, 13),
-                    new Point(41, 40),
-                    new Point(61, 41),
-                    new Point(32, 46)
-                    };
-
-            // Adds the rhombus to the graphics path
-
-            pctBox_Path.AddPolygon(Rhombus);
-
-            //the region of the picturebox is now set to the shape of the graphics path
-            pctBox_BlueHold.Region = new Region(pctBox_Path);
-        }
-
-        private void pctBox_RedHoldButton_Click(object sender, EventArgs e)
-        {
-            Green = false;
-            Red = true;
-            Blue = false;
-        }
-
-        public void pctBox_RedHold_Paint(object sender, PaintEventArgs e)
-        {
-
-            pctBox_RedHold.BackColor = Color.Red;
-
-            GraphicsPath pctBox_Path = new GraphicsPath();
-
-            // The points of the rhombus in n array, creates the polygon
-            Point[] Rhombus =
-                {
+                case "Red":
+                    Point[] RedShape =
+                    {
                     new Point(-23, -10),
                     new Point(20, 5),
                     new Point(26, 10),
                     new Point(25, 20),
                     new Point(15, 20)
                     };
+                    HoldShape = RedShape;
+                    break;
+                    
+                case "Blue":
+                    Point[] BlueShape =
+                    {
+                    new Point(-2, 12),
+                    new Point(32, 13),
+                    new Point(41, 40),
+                    new Point(61, 41),
+                    new Point(32, 46)
+                    };
+                    HoldShape = BlueShape;
+                    break;
+                    
+                default:
+                    // Default case.
+                    Point[] GreenShape =
+                    {
+                    new Point(23, 20),
+                    new Point(40, 10),
+                    new Point(57, 20),
+                    new Point(50, 40),
+                    new Point(30, 40)
+                    };
+                    HoldShape = GreenShape;
+                    break;
+            }
 
             // Adds the rhombus to the graphics path
 
-            pctBox_Path.AddPolygon(Rhombus);
+            pctBox_Path.AddPolygon(HoldShape);
 
             //the region of the picturebox is now set to the shape of the graphics path
-            pctBox_RedHold.Region = new Region(pctBox_Path);
+            pctBox_CurrentHold.Region = new Region(pctBox_Path);
         }
     }
 }
