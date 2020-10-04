779234e4ce529772f48a57b7bf215fdca257fee6
max larner <06mlarner@brightoncollege.net>
2020-10-04 06:45:48 -0700

    Published to GitHub

---
+++ Computer Science Coursework/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace Computer_Science_Coursework
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btn_WallSubmit = new System.Windows.Forms.Button();
+            this.txt_WallWidth = new System.Windows.Forms.TextBox();
+            this.txt_WallHeight = new System.Windows.Forms.TextBox();
+            this.pnl_Holds = new System.Windows.Forms.Panel();
+            this.pctBox_GreenHoldButton = new System.Windows.Forms.PictureBox();
+            this.pctBox_BlueHoldButton = new System.Windows.Forms.PictureBox();
+            this.pctBox_RedHoldButton = new System.Windows.Forms.PictureBox();
+            this.pnl_Holds.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pctBox_GreenHoldButton)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctBox_BlueHoldButton)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctBox_RedHoldButton)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_WallSubmit
+            // 
+            this.btn_WallSubmit.Location = new System.Drawing.Point(98, 218);
+            this.btn_WallSubmit.Name = "btn_WallSubmit";
+            this.btn_WallSubmit.Size = new System.Drawing.Size(89, 31);
+            this.btn_WallSubmit.TabIndex = 0;
+            this.btn_WallSubmit.Text = "Submit ";
+            this.btn_WallSubmit.UseVisualStyleBackColor = true;
+            this.btn_WallSubmit.Click += new System.EventHandler(this.btn_WallSubmit_Click);
+            // 
+            // txt_WallWidth
+            // 
+            this.txt_WallWidth.Location = new System.Drawing.Point(98, 157);
+            this.txt_WallWidth.Name = "txt_WallWidth";
+            this.txt_WallWidth.Size = new System.Drawing.Size(89, 23);
+            this.txt_WallWidth.TabIndex = 1;
+            // 
+            // txt_WallHeight
+            // 
+            this.txt_WallHeight.Location = new System.Drawing.Point(98, 186);
+            this.txt_WallHeight.Name = "txt_WallHeight";
+            this.txt_WallHeight.Size = new System.Drawing.Size(89, 23);
+            this.txt_WallHeight.TabIndex = 1;
+            // 
+            // pnl_Holds
+            // 
+            this.pnl_Holds.Controls.Add(this.pctBox_GreenHoldButton);
+            this.pnl_Holds.Controls.Add(this.pctBox_BlueHoldButton);
+            this.pnl_Holds.Controls.Add(this.pctBox_RedHoldButton);
+            this.pnl_Holds.Location = new System.Drawing.Point(280, 560);
+            this.pnl_Holds.Name = "pnl_Holds";
+            this.pnl_Holds.Size = new System.Drawing.Size(680, 160);
+            this.pnl_Holds.TabIndex = 2;
+            // 
+            // pctBox_GreenHoldButton
+            // 
+            this.pctBox_GreenHoldButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.pctBox_GreenHoldButton.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pctBox_GreenHoldButton.BackgroundImage")));
+            this.pctBox_GreenHoldButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pctBox_GreenHoldButton.Location = new System.Drawing.Point(480, 26);
+            this.pctBox_GreenHoldButton.Name = "pctBox_GreenHoldButton";
+            this.pctBox_GreenHoldButton.Size = new System.Drawing.Size(125, 119);
+            this.pctBox_GreenHoldButton.TabIndex = 2;
+            this.pctBox_GreenHoldButton.TabStop = false;
+            this.pctBox_GreenHoldButton.Click += new System.EventHandler(this.pctBox_GreenHoldButton_Click);
+            // 
+            // pctBox_BlueHoldButton
+            // 
+            this.pctBox_BlueHoldButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.pctBox_BlueHoldButton.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pctBox_BlueHoldButton.BackgroundImage")));
+            this.pctBox_BlueHoldButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pctBox_BlueHoldButton.Location = new System.Drawing.Point(317, 26);
+            this.pctBox_BlueHoldButton.Name = "pctBox_BlueHoldButton";
+            this.pctBox_BlueHoldButton.Size = new System.Drawing.Size(133, 119);
+            this.pctBox_BlueHoldButton.TabIndex = 1;
+            this.pctBox_BlueHoldButton.TabStop = false;
+            this.pctBox_BlueHoldButton.Click += new System.EventHandler(this.pctBox_BlueHoldButton_Click);
+            // 
+            // pctBox_RedHoldButton
+            // 
+            this.pctBox_RedHoldButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.pctBox_RedHoldButton.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pctBox_RedHoldButton.BackgroundImage")));
+            this.pctBox_RedHoldButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pctBox_RedHoldButton.Location = new System.Drawing.Point(147, 26);
+            this.pctBox_RedHoldButton.Name = "pctBox_RedHoldButton";
+            this.pctBox_RedHoldButton.Size = new System.Drawing.Size(141, 119);
+            this.pctBox_RedHoldButton.TabIndex = 0;
+            this.pctBox_RedHoldButton.TabStop = false;
+            this.pctBox_RedHoldButton.Click += new System.EventHandler(this.pctBox_RedHoldButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1184, 761);
+            this.Controls.Add(this.pnl_Holds);
+            this.Controls.Add(this.txt_WallHeight);
+            this.Controls.Add(this.txt_WallWidth);
+            this.Controls.Add(this.btn_WallSubmit);
+            this.Name = "Form1";
+            this.Text = "Wall Build";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.pnl_Holds.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pctBox_GreenHoldButton)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctBox_BlueHoldButton)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctBox_RedHoldButton)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_WallSubmit;
+        private System.Windows.Forms.TextBox txt_WallWidth;
+        private System.Windows.Forms.TextBox txt_WallHeight;
+        private System.Windows.Forms.Panel pnl_Holds;
+        private System.Windows.Forms.PictureBox pctBox_BlueHoldButton;
+        private System.Windows.Forms.PictureBox pctBox_GreenHoldButton;
+        private System.Windows.Forms.PictureBox pctBox_RedHoldButton;
+    }
+}
+
---
+++ Computer Science Coursework/Form1.cs
@@ -0,0 +1,211 @@
+﻿using DocumentFormat.OpenXml.InkML;
+using GemBox.Document.Drawing;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
+
+namespace Computer_Science_Coursework
+{
+    public partial class Form1 : Form
+    {
+        int HoldCount = 0;
+        //Bool values to decide which hold has been selected
+        bool Red = false;
+        bool Green = false;
+        bool Blue = false;
+
+        //Defines the wall panel 
+        Panel WallPanel = new Panel();
+        //Defines the picture boxes for each hold "is there a better way to do this? revisit..."
+        PictureBox pctBox_GreenHold = new PictureBox();
+        PictureBox pctBox_BlueHold = new PictureBox();
+        PictureBox pctBox_RedHold = new PictureBox();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_WallSubmit_Click(object sender, EventArgs e)
+        {
+            int WallWidth = int.Parse(txt_WallWidth.Text);
+            int WallHeight = int.Parse(txt_WallHeight.Text);
+
+            MessageBox.Show(txt_WallWidth.Text + ", " + txt_WallHeight.Text, "Wall Size");
+            //Passes The desired height and Width of the wall to the create wall Function
+            CreateWall(WallWidth, WallHeight);
+        }
+
+        private void CreateWall(int X, int Y)
+        { 
+            // Create a panel called "WallPanel@
+            WallPanel.Location = new System.Drawing.Point(300, 50);
+            WallPanel.Name = "pnl_ClimbingWall";
+            // Assigns the size the dimentions passed to the subroutine...
+            //...from WallWidth and Wall Height in the WallSubmit click event
+            WallPanel.Size = new System.Drawing.Size(X, Y);
+            WallPanel.BackColor = Color.LightGreen;
+            //Dynamically Creates a click event for "WallPanel" to dectect when its been clicked 
+            WallPanel.MouseClick+=WallPanel_Click;
+            //Adds "WallPanel" to the form
+            Controls.Add(WallPanel);
+        }
+
+        private void AddHold(int x, int y, PictureBox pctBox_Hold)
+        {
+            //Asigns the picturebox passed in (the Hold) a numbered name 
+            pctBox_Hold.Name = "pctBox_Hold" + Convert.ToString(HoldCount);
+            //Assigns the location of the picturebox on the wall, position passed in from the WallPanel Click event
+            pctBox_Hold.Location = new Point(x, y);
+            //increases the hold count to make sure the next hold added has a unique name
+            HoldCount++;
+            //Adds the hold to the WallPanel
+            WallPanel.Controls.Add(pctBox_Hold);
+        }
+        // Onclick event for WallPanel
+        private void WallPanel_Click(object sender, MouseEventArgs e)
+        {
+            //Dectects the X and Y co-ordinates of the click and assigns them to variables
+            int xMouseClick = e.X;
+            int yMouseClick = e.Y;
+
+            //Used for my own testing perpouses, not a feature 
+            MessageBox.Show(Convert.ToString(xMouseClick), Convert.ToString(yMouseClick));
+
+            //Creates a temporary picturebox that can be assigned the value of a hold picturebox... 
+            //...This is so any picture box can be passed into the add hold function
+            PictureBox pctBox_Temp = new PictureBox();
+            
+            if (Blue == true)
+            {
+                pctBox_Temp = pctBox_BlueHold;
+                //paints the picturebox with a unique shape to the Form
+                //the picturebox itself is added to the panel in the AddHold() Function
+                pctBox_Temp.Paint += new PaintEventHandler(pctBox_BlueHold_Paint);
+            }
+            else if (Green == true)
+            {
+                pctBox_Temp = pctBox_GreenHold;
+                pctBox_Temp.Paint += new PaintEventHandler(pctBox_GreenHold_Paint);
+            }
+            else if (Red == true)
+            {
+                pctBox_Temp = pctBox_RedHold;
+                pctBox_Temp.Paint += new PaintEventHandler(pctBox_RedHold_Paint);
+            }
+
+            AddHold(xMouseClick, yMouseClick, pctBox_Temp);
+        }
+        
+        //onclick event and paint event for every button... There must be a better way?? investigate using OOP to solve problem
+        public void pctBox_GreenHoldButton_Click(object sender, EventArgs e)
+        { 
+            //When the Greenhold button has been selected, it sets Green to true and others to false 
+            Green = true;
+            Red = false;
+            Blue = false;
+        }
+
+        private void pctBox_GreenHold_Paint(object sender, PaintEventArgs e)
+        {
+            pctBox_GreenHold.BackColor = Color.Green;
+
+            //Creates a new graphics path that the hold shape can be added to
+            GraphicsPath pctBox_Path = new GraphicsPath();
+
+            // Creates a polygon using an array of points called GreenShape
+
+            Point[] GreenShape =
+                {
+                    new Point(23, 20),
+                    new Point(40, 10),
+                    new Point(57, 20),
+                    new Point(50, 40),
+                    new Point(30, 40)
+                    };
+
+            // Adds the polygon to the graphics path
+            pctBox_Path.AddPolygon(GreenShape);
+
+            //the region of the picturebox is now set to the shape of the graphics path
+            pctBox_GreenHold.Region = new Region(pctBox_Path);
+        }
+
+        public void pctBox_BlueHoldButton_Click(object sender, EventArgs e)
+        {
+            Green = false;
+            Blue = true;
+            Red = false;
+        }
+
+        private void pctBox_BlueHold_Paint(object sender, PaintEventArgs e)
+        {
+            pctBox_BlueHold.BackColor = Color.Blue;
+
+            GraphicsPath pctBox_Path = new GraphicsPath();
+
+            // The points of the rhombus in n array, creates the polygon
+            Point[] Rhombus =
+                {
+                    new Point(-2, 12),
+                    new Point(32, 13),
+                    new Point(41, 40),
+                    new Point(61, 41),
+                    new Point(32, 46)
+                    };
+
+            // Adds the rhombus to the graphics path
+
+            pctBox_Path.AddPolygon(Rhombus);
+
+            //the region of the picturebox is now set to the shape of the graphics path
+            pctBox_BlueHold.Region = new Region(pctBox_Path);
+        }
+
+        private void pctBox_RedHoldButton_Click(object sender, EventArgs e)
+        {
+            Green = false;
+            Red = true;
+            Blue = false;
+        }
+
+        public void pctBox_RedHold_Paint(object sender, PaintEventArgs e)
+        {
+
+            pctBox_RedHold.BackColor = Color.Red;
+
+            GraphicsPath pctBox_Path = new GraphicsPath();
+
+            // The points of the rhombus in n array, creates the polygon
+            Point[] Rhombus =
+                {
+                    new Point(-23, -10),
+                    new Point(20, 5),
+                    new Point(26, 10),
+                    new Point(25, 20),
+                    new Point(15, 20)
+                    };
+
+            // Adds the rhombus to the graphics path
+
+            pctBox_Path.AddPolygon(Rhombus);
+
+            //the region of the picturebox is now set to the shape of the graphics path
+            pctBox_RedHold.Region = new Region(pctBox_Path);
+        }
+    }
+}
